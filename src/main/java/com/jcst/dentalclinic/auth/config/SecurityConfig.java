a67d96d58f56636b845df9f1773656aa93ea3230
juansierra24 <carlos.sierratrujillo@gmail.com>
2023-05-01 21:36:50 -0500

    refactor SecurityConfig

--- src/main/java/com/jcst/dentalclinic/auth/config/SecurityConfig.java
+++ src/main/java/com/jcst/dentalclinic/auth/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.jcst.dentalclinic.auth.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -22,24 +23,13 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
 
-        http.csrf()
-                .disable()
-                .authorizeHttpRequests()
-                .requestMatchers(
-                        new AntPathRequestMatcher("/api/v1/auth/**"),
-                        new AntPathRequestMatcher("/v3/api-docs/**"),
-                        new AntPathRequestMatcher("/swagger-ui/*"),
-                        new AntPathRequestMatcher("/swagger-config/*"))
-                .permitAll()
-                .anyRequest()
-                .authenticated()
-                .and()
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-                .cors()
-                .and()
-                .authenticationProvider(authenticationProvider)
+        http.csrf().disable().authorizeHttpRequests()
+                .antMatchers("/v3/api-docs/**", "/swagger-ui/*", "/swagger-config/*" ).permitAll()
+                .antMatchers(HttpMethod.POST, "/api/v1/auth/**").permitAll()
+                .anyRequest().authenticated()
+                .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and().cors()
+                .and().authenticationProvider(authenticationProvider)
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
